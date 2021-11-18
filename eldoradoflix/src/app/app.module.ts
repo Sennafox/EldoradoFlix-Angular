7f57e0cb7fda1d89f01eee3f4a61f7ed19a195db
PAULO BITTENCOURT/LGESP TV SQA Part - MAO(paulo.bittencourt@lge.com) <paulo.bittencourt@lge.com>
2021-11-18 15:11:43 -0400

    Construção do template da página home

---
+++ eldoradoflix/src/app/app.module.ts
@@ -0,0 +1,32 @@
+import { CardComponent } from './shared/grade/card/card.component';
+import { SeletorComponent } from './shared/grade/seletor/seletor.component';
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { NavBarComponent } from './shared/nav-bar/nav-bar.component';
+import { BannerComponent } from './shared/banner/banner.component';
+import { GradeComponent } from './shared/grade/grade.component';
+import { FooterComponent } from './shared/footer/footer.component';
+import { PaginationComponent } from './shared/pagination/pagination.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    NavBarComponent,
+    BannerComponent,
+    GradeComponent,
+    SeletorComponent,
+    CardComponent,
+    FooterComponent,
+    PaginationComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ eldoradoflix/src/app/shared/grade/seletor/seletor.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-seletor',
+  templateUrl: './seletor.component.html',
+  styleUrls: ['./seletor.component.scss']
+})
+export class SeletorComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
