e84d44cdcdeb42aafaa8268ff2ab84b8415692b8
Sennafox <pstbittencout@gmail.com>
2021-11-18 22:43:53 -0400

    atualizando o scss

--- eldoradoflix/src/app/shared/grade/card/card.component.ts
+++ eldoradoflix/src/app/shared/grade/card/card.component.ts
@@ -7,6 +7,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CardComponent implements OnInit {
 
+  public listas : any = [
+    {title: 'Rambo 1'},
+    {title: 'Rambo 2'},
+    {title: 'Rambo 3'},
+    {title: 'Rambo 4'},
+    {title: 'Rambo 5'},
+    
+  ]
+
   constructor() { }
 
   ngOnInit(): void {
